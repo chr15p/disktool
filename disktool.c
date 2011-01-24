bbd30ac7b8f66c12492eda399699cbf280ff4898
chrisp <chris.procter@talk21.com>
2011-01-24 21:55:04 +0000

    Fix the annoying "showing new dm device after adding new sd devices" bug, essentially the detection of new devices was not respecting the filters

--- disktool.c
+++ disktool.c
@@ -193,7 +193,8 @@ int main(int argc,char *argv[])
 				event=&buf[j];
 				k=0;
 				while(sections[k].path!=NULL){
-		        	        if(strncmp(event->name,sections[k].filter,strlen(sections[k].filter))==0){
+		        	        if((sections[i].flags & displayflags)
+						&&(strncmp(event->name,sections[k].filter,strlen(sections[k].filter))==0)){
 						//printf("added %s %s\n",event->name,sections[k].filter);
 						create_entity(&dev_list,"/sys/block/",event->name,&(sections[k]),displayflags,"(added)");
 						
